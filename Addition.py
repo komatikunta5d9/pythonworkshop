4e2f04c7aabd76ba29c02524d3f9b7c169791181
komatikunta5d9 <indukomatikunta@gmail.com>
2021-01-29 15:15:34 +0530

    first commit

---
+++ Addition.py
@@ -0,0 +1,3 @@
+a=int(input('Enter'))
+B=int(input('Enter'))
+print('Addition Value is :-',a+b)
---
+++ sample1.py
@@ -0,0 +1,72 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=100
+>>> type(a)
+<class 'int'>
+>>> ch='c'
+>>> type(ch)
+<class 'str'>
+>>> input()
+
+''
+>>> input()
+
+''
+>>> input()
+"indu"
+'"indu"'
+>>> a=input()
+2909
+>>> a
+'2909'
+>>> type(a)
+<class 'str'>
+>>> a=int(input())
+929
+>>> type(a)
+<class 'int'>
+>>> a=float(input())
+29.09
+>>> type(a)
+<class 'float'>
+>>> a=float(input('Enter Value:-'))
+Enter Value:-290926.2728141
+>>> a
+290926.2728141
+>>> type(a)
+<class 'float'>
+>>> a=int('29')
+>>> 
+>>> a
+29
+>>> a=int('29 ')
+>>> a
+29
+>>> a=int(29.928)
+>>> a
+29
+>>> a=float(09)
+SyntaxError: invalid token
+>>> a=float(10)
+>>> a
+10.0
+>>> a=str(20)
+>>> a
+'20'
+>>> print('a')
+a
+>>> print(a)
+20
+>>> a=2
+>>> b=5
+>>> print(a+b)
+7
+>>> print(addition value)
+SyntaxError: invalid syntax
+>>> a=int(input('Enter'))
+Enter5
+>>> b=int(input('Enter'))
+Enter10
+>>> print('Addition Value is:-',a+b)
+Addition Value is:- 15
+>>> 
